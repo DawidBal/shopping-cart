96e6314d0ba43be0141b0891f122c46c0a0af73b
Dawid <baluszynskidawid@gmail.com>
2021-05-25 22:44:51 +0200

    feat: add loading state while fetching data

--- src/components/Utilities/useFetchData.js
+++ src/components/Utilities/useFetchData.js
@@ -2,15 +2,24 @@ import  { useState, useEffect } from 'react';
 
 export default function useFetchData(url) {
     const [data, setData] = useState([])
+    const [loading, setLoading] = useState(false);
+
     const fetchData = async () => {
-        const fetcher = await fetch(url);
-        const data = await fetcher.json();
-        setData(data);
+        try {
+            setLoading(true);
+          const fetcher = await fetch(url);
+          const data = await fetcher.json();
+          setData(data);
+          setLoading(false);
+          } catch {
+            setLoading(false);
+            throw new Error("Error!");
+          }
     }
 
     useEffect(() => {
         fetchData();
     }, [])
 
-    return data
+    return [loading, data]
 }
