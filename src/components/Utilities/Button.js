a3e13477f3de2f5f14f0b703659524f7ebc7d459
Dawid <baluszynskidawid@gmail.com>
2021-05-25 22:54:27 +0200

    feat: create utility button component

---
+++ src/components/Utilities/Button.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Btn = styled.button`
+
+    position: relative;
+    width: 200px;
+    padding: 0.5rem 1rem;
+
+    display: grid;
+    place-items: center;
+    text-align: center;
+    
+    color: #fff;
+    font-size: 1.2rem;
+    background-color: transparent;
+    border: 2px solid ${props => props.cart ? '#fff' : '#0e63d8'};
+    z-index: 0;
+    cursor: pointer;
+    transition: transform 200ms ease-out;
+
+    &::before {
+        content: '';
+        position: absolute;
+        left: -.6rem;
+        top: -.5rem;
+        width: 100%;
+        height: 100%;
+        z-index: -1;
+        background-color: #0e63d8;
+        transition: all 200ms ease-out;
+        
+    }
+
+    &:active {
+        transform: scale(0.9);
+    }
+
+    &:hover::before {
+        top: 0;
+        left: 0;
+    }
+`
+
+const Button = ({ children, onClick }) => {
+    return (
+        <Btn onClick={onClick}>
+            {children}
+        </Btn>
+    )
+}
+
+export default Button
