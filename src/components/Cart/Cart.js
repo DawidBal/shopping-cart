5dde52f0c32e7e4be2774102e8286b8dda35c3b5
Dawid <baluszynskidawid@gmail.com>
2021-05-27 18:58:22 +0200

    feat: add order calculations based on items in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import Button from '../Utilities/Button'
 import CartProduct from './CartProduct'
@@ -29,30 +29,54 @@ const Wrapper = styled.ul`
 const Summary = styled.aside`
     padding: 1rem 2rem;
     display: flex;
+    gap: 1rem;
     flex-direction: column;
     align-items: center;
     background: #f0f0f0;
 `
 
-const Cart = ( {cartItems} ) => {
+const Cart = ({ cartItems, setCartItems } ) => {
+
+    const [orderValue, setOrderValue] = useState(0.00);
+    const [isDeliveryFree, setIsDeliveryFree] = useState(false);
+    const freeDeliveryValue = 199.99;
+
+    const checkDelivery = () => orderValue >= freeDeliveryValue ? setIsDeliveryFree(true) : setIsDeliveryFree(false);
+
+    const calculateOrderValue = () => {
+        setOrderValue(cartItems.length > 0 ? cartItems.reduce((sum, item) => {
+            return +(sum += item.price * item.quantity).toFixed(2);
+        }, 0) : 0.00)
+    }
+
+    const calculateTotalValue = () => isDeliveryFree ? `${orderValue}$` : `${orderValue + 9.99}$`;
+    
+    useEffect(() => {
+        calculateOrderValue();
+    }, [cartItems])
+
+    useEffect(() => {
+        checkDelivery();
+    }, [orderValue])
+
     return (
         <Main>
             <Title>Cart</Title>
             <Wrapper>
-                {cartItems.map(item => 
-                        <CartProduct key={item.id + item.size} data={item}/>
+                {cartItems.map((item, index) => 
+                    <CartProduct key={item.id + item.size} data={item} cartItems={cartItems} itemIndex={index} setCartItems={setCartItems}/>
                     )}
             </Wrapper>
             <Summary>
                 <h2>Summary</h2>
                 <div>
                     <div>
-                    <p>Order value: </p>
-                    <p>Delivery: </p>
+                        <p>{`Order value: ${orderValue}$`}</p>
+                        <p>Delivery: </p>
                     </div>
-                <p>Total: </p>
+                    <p>Total: { calculateTotalValue() }</p>
                 </div>
-                <Button>Checkout</Button>
+                <Button cart>Checkout</Button>
             </Summary>
         </Main>
     )
