300e77d0a7ce1521b8e3257f536901ce40ebd044
Dawid <baluszynskidawid@gmail.com>
2021-06-05 14:07:08 +0200

    feat: add overlay in home component to improve text readability

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,6 +21,7 @@ const Wrapper = styled.div`
     gap: ${props => props.links ? "1.5rem" : "0.5rem"};
     flex-wrap: wrap;
     text-align: center;
+     ${props => props.column ? "line-height: 1" : null};
 `
 
 const Title = styled.h2`
@@ -38,18 +39,32 @@ const ColorText = styled.span`
     color: var(--main-color);
 `
 
+const Overlay = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 1.5rem;
+    flex-wrap: wrap;
+    text-align: center;
+    background: linear-gradient(90deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.7) 50%, rgba(0,0,0,0) 100%);
+    padding: 2rem 0;
+`
+
 const Home = () => {
     return (
         <Main>
-            <Wrapper column>
-                <Title>Because you have to <ColorText>wear some</ColorText>thing</Title>
-                <SubTitle>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, perspiciatis?
-                </SubTitle>
-            </Wrapper>
-            <Wrapper links>
-                <ButtonLink to="/shop">Visit Shop</ButtonLink>
-                <ButtonLink to="/cart">Checkout Cart</ButtonLink>
-            </Wrapper>
+            <Overlay>
+                <Wrapper column>
+                    <Title>Because you have to <ColorText>wear some</ColorText>thing</Title>
+                    <SubTitle>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, perspiciatis?
+                    </SubTitle>
+                </Wrapper>
+                <Wrapper links>
+                    <ButtonLink to="/shop">Visit Shop</ButtonLink>
+                    <ButtonLink to="/cart">Checkout Cart</ButtonLink>
+                </Wrapper>
+            </Overlay>
         </Main>
     )
 }
